6785e0ef06c2243e02cd920f8f708456d5bfc588
Monica <tudormonica1996@yahoo.com>
2026-01-08 21:12:52 +0200

    Fix Main

--- main.cpp
+++ main.cpp
@@ -13,13 +13,8 @@ int main()
     vector<Ride> rides;
     vector<shared_ptr<Driver>> drivers;
     Menu menu(users,drivers,rides);
-    //menu.run();
+    menu.run();
 
-    Location l(8.0,9.0);
-    Location l2(10.0,11.0);
-    Location l3;
-    cout<<l.distanceTo(l2);
-    return 0;
 
 
 
